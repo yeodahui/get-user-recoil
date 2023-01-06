2ee80e18b7e77e1d689ee6b6852b091a69bfdbc3
여다희 <yeodahui.dev@gmail.com>
2023-01-07 02:09:36 +0900

    추가: Async Selector를 이용한 비동기 데이터 상태 관리

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import "./App.css";
+import React from "react";
+import { useRecoilValue } from "recoil";
+import { userSelctor } from "./userState";
 
 function App() {
-  return <div className="App"></div>;
+  const user = useRecoilValue(userSelctor);
+
+  return (
+    <div className="App">
+      <div className="user">
+        <React.Suspense fallback={"loading..."}>
+          <img
+            src={`${user.picture.large}`}
+            alt="profile"
+            className="user-img"
+            loading="lazy"
+          />
+          <strong className="user-name">
+            {user.name.first + " " + user.name.last}
+          </strong>
+          <span className="user-email">{user.email}</span>
+        </React.Suspense>
+      </div>
+    </div>
+  );
 }
 
 export default App;
