7486a5ad0cf4a79d9f145dafbcd375b322a17c32
여다희 <yeodahui.dev@gmail.com>
2023-01-07 02:09:08 +0900

    추가: recoil 설치 및 환경설정

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+// import App from "./App2";
+// import App from "./App3";
+import { RecoilRoot } from "recoil";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RecoilRoot>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </RecoilRoot>
 );
